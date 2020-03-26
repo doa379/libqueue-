a209d65fe3daa941bc6a4b677dcfb76c81d9334e
doa379 <doa379@gmail.com>
2020-03-26 10:49:38 +0000

    Exercise in lambda functions

--- test_tpool.cpp
+++ test_tpool.cpp
@@ -7,9 +7,9 @@
 #include <unistd.h>
 #include "tpool.hpp"
 
-void func_cb(int v)
+void func_cb(const int *v)
 {
-  std::cout << v << std::endl;
+  std::cout << *v << std::endl;
   sleep(5);
   std::cout << "Job exit" << std::endl;
 }
@@ -18,19 +18,25 @@ int main()
 {
   Tpool tpool;
   srand(0);
+  std::vector<int> V;
 
   for (unsigned i = 0; i < 5; i++)
-    {
-      int v = rand() % 100;
-      tpool.add_job([v]() { func_cb(v); });
-      std::cout << "Data " << v << "job added" << std::endl;
-    }
+  {
+    int v = rand() % 100;
+    V.emplace_back(std::move(v));
+  }
+
+  for (int i = 0; i < V.size(); i++)
+  {
+    tpool.add_job([&V, i]() { func_cb(&V.at(i)); });
+    std::cout << "Data " << V.at(i) << "job added" << std::endl;
+  }
 
   while (tpool.job_count())
-    {
-      std::cout << tpool.job_count() << " jobs remaining" << std::endl;
-      sleep(1);
-    }
+  {
+    std::cout << tpool.job_count() << " jobs remaining" << std::endl;
+    sleep(1);
+  }
 
   return 0;
 }
