f03a8db97f1b6ffd1b31757cb7a1c979e1940b8c
doa379 <doa379@gmail.com>
2020-06-09 23:19:55 +0100

    ;

--- tpool.cpp
+++ tpool.cpp
@@ -16,7 +16,7 @@ Tpool::~Tpool(void)
 void Tpool::clear_jobs(void)
 {
   std::lock_guard<std::mutex> lock(q_mtx);
-  while(!q.empty()) q.pop();
+  while (!q.empty()) q.pop();
   cv.notify_one();
 }
 
@@ -28,12 +28,10 @@ void Tpool::worker(void)
   {
     {
       std::unique_lock<std::mutex> lock(q_mtx);
-
-      while (q.empty() && !quit) // Sleeping
-        cv.wait(lock);
+      cv.wait(lock, [this](){ return !q.empty() || quit; });
 
       if (quit)
-        break;
+        return;
 
       curr_job = q.front();
     }
@@ -49,21 +47,18 @@ void Tpool::worker(void)
 
 void Tpool::add_job(std::function<void()> job)
 {
-  std::unique_lock<std::mutex> lock(q_mtx);
+  if (quit)
+    return;
 
-  if (!quit)
-  {
-    q.push(job);
-    cv.notify_one();
-  }
+  std::lock_guard<std::mutex> lock(q_mtx);
+  q.push(job);
+  cv.notify_one();
 }
 
 size_t Tpool::job_count(void)
 {
-  size_t count;
   std::lock_guard<std::mutex> lock(q_mtx);
-  count = q.size();
-  return count;
+  return q.size();
 }
 
 void Tpool::suspend(void)
